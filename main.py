cdb8b59a2256d9f0bcca763635e8d6c44e5898d5
emersonslimaa <emersonsl1605@gmail.com>
2020-09-20 15:13:02 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+arquivo = pd.read_csv('breast-cancer.csv', names=["id_number", "clump_thickness", "uniformity_of_cell_size", "uniformity_of_cell_shape", "marginal_adhesion", "single_epithelial_cell_size", "bare_nuclei" ,"bland_chromatin", "normal_nucleoli", "mitoses", "class"])
+
+print(arquivo)
