810e6f01598e19666584a2ee021193793d860501
emersonslimaa <emersonsl1605@gmail.com>
2020-09-20 15:17:18 -0300

    add comment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+#Abertura do arquivo
+
 import pandas as pd
 
 arquivo = pd.read_csv('breast-cancer.csv', names=["id_number", "clump_thickness", "uniformity_of_cell_size", "uniformity_of_cell_shape", "marginal_adhesion", "single_epithelial_cell_size", "bare_nuclei" ,"bland_chromatin", "normal_nucleoli", "mitoses", "class"])
 
 print(arquivo)
+
